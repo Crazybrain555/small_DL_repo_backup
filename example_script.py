862ced683009c1601670b9e2aabc9e9350cf4aee
rui yang zhang <98039865+Crazybrain555@users.noreply.github.com>
2023-08-01 21:39:20 +0800

    add

---
+++ example_script.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+
+import numpy as np
+from torch import nn
+
+
+def this_is_function(x):
+    """
+    And this is doc
+    """
+    return x / 2
+
+
+print("Hello world!")
+print(2 + 2 / 4)
